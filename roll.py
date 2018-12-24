8307b77a56a0a7977cd9ddaf8776b49c4dd028a8
Zixuan Wang <wangzixuanzju@gmail.com>
2018-12-24 13:43:05 -0500

    zx

---
+++ roll.py
@@ -0,0 +1,10 @@
+ def rollDice():
+    ...:     roll=random.randint(1,100)
+    ...:     return roll
+    ...: x = 0
+    ...: while x<100:
+    ...:     result=rollDice()
+    ...:     print result
+    ...:     x+=1
+
+
